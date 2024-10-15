0b454b53e83affccabe32ceb06d4a11f593d53c9
Anton Teerioja <anton.s.teerioja@utu.fi>
2024-10-15 16:02:38 +0300

    Complete 1.6

--- part1/uniCafe/src/App.jsx
+++ part1/uniCafe/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 
+const Header = ({name}) => <h1>{name}</h1>
+
 const App = () => {
   // save clicks of each button to its own state
+  const header = "give feedback"
+  const header2 = "statistics"
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
 
   return (
     <div>
-      code here
+      <Header name={header} />
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Header name={header2} />
+      <p> good {good} </p>
+      <p> neutral {neutral} </p>  
+      <p> bad {bad} </p> 
+      
     </div>
   )
 }
