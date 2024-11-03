9550dc451d95b500d6cf61646d41779e8bd0c829
Anton Teerioja <anton.s.teerioja@utu.fi>
2024-11-03 21:46:09 +0200

    Complete 1.7 & 1.8

--- part1/uniCafe/src/App.jsx
+++ part1/uniCafe/src/App.jsx
@@ -2,6 +2,24 @@ import { useState } from 'react'
 
 const Header = ({name}) => <h1>{name}</h1>
 
+const Statistics = (props) => {
+  
+  const sum = props.good + props.neutral + props.bad 
+  const average = ((props.good * 1 + props.neutral * 0 + props.bad * -1) / sum)
+  const positive = (props.good / sum) * 100
+
+  return (
+  <>
+    <p> good {props.good} </p>
+    <p> neutral {props.neutral} </p>
+    <p> bad {props.bad} </p>
+    <p> all {sum} </p>
+    <p> average {average}</p>
+    <p> positive {positive} %</p>
+  </>  
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const header = "give feedback"
@@ -10,6 +28,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
   
 
   return (
@@ -19,10 +38,8 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <Header name={header2} />
-      <p> good {good} </p>
-      <p> neutral {neutral} </p>  
-      <p> bad {bad} </p> 
-      
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
     </div>
   )
 }
